050c7895d8def8c31bf977622fc556a7ad0992da
Raphael Silva <raphael_silva07@hotmail.com>
2016-01-29 05:38:21 -0200

    javafx cdc

---
+++ Eclipse/cdc - FormularioLogin/src/LoginApp.java
@@ -0,0 +1,55 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(400, 300);
+
+		// componentes
+		TextField txLogin = new TextField();
+		txLogin.setPromptText("Digite aqui seu login");
+
+		PasswordField txSenha = new PasswordField();
+		txSenha.setPromptText("Digite aqui sua senha");
+
+		Button btEntrar = new Button("Entrar");
+		Button btSair = new Button("Sair");
+
+		// adicionando componentes ao painel
+		pane.getChildren().addAll(txLogin, txSenha, btEntrar, btSair);
+
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.show();
+
+		// posicionando os componentes
+		txLogin.setLayoutX((pane.getWidth() - txLogin.getWidth()) / 2);
+		txLogin.setLayoutY(50);
+
+		txSenha.setLayoutX((pane.getWidth() - txSenha.getWidth()) / 2);
+		txSenha.setLayoutY(100);
+
+		btEntrar.setLayoutX((pane.getWidth() - btEntrar.getWidth()) / 2);
+		btEntrar.setLayoutY(150);
+
+		btSair.setLayoutX((pane.getWidth() - btSair.getWidth()) / 2);
+		btSair.setLayoutY(200);
+
+		/* Nosso primeiro toque de requinte... */
+		pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, blue 0%, silver 100%);");
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
